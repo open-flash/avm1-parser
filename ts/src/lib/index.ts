6bb1a50b931fe91254b8a4813c33fa92b05e5616
Charles Samborski <demurgos@demurgos.net>
2018-11-07 00:00:17 +0100

    [ts] Update API to allow reading only a single action

--- ts/src/lib/index.ts
+++ ts/src/lib/index.ts
@@ -1,8 +1,25 @@
 import { ReadableStream } from "@open-flash/stream";
 import { Action } from "avm1-tree";
-import { parseActionString } from "./parsers/avm1";
+import { UintSize } from "semantic-types";
+import { parseAction } from "./parsers/avm1";
 
-export function parseBytes(bytes: Uint8Array): Action[] {
-  const byteStream: ReadableStream = new ReadableStream(bytes);
-  return parseActionString(byteStream);
+export class Avm1Parser {
+  private readonly stream: ReadableStream;
+
+  constructor(bytes: Uint8Array) {
+    this.stream = new ReadableStream(bytes);
+  }
+
+  public getBytePos(): UintSize {
+    return this.stream.bytePos;
+  }
+
+  readNext(): Action | undefined {
+    return parseAction(this.stream);
+  }
+
+  readAt(offset: UintSize): Action | undefined {
+    this.stream.bytePos = offset;
+    return parseAction(this.stream);
+  }
 }
